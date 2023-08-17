b01db0d55bcf3e68fc1cea0d2d329cfe25cd3b26
zxlw68 <zxlw68@hotmail.com>
2023-08-17 05:16:48 -0500

    login no user with that username,  return users.find

--- passport-config.js
+++ passport-config.js
@@ -2,8 +2,12 @@ const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail) {
-  const authenticateUser = async (email, passport, done) => {
+  const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
+    // const user = users.find((user) => user.email === email)
+    console.log(getUserByEmail)
+    console.log(user)
+    console.log(email)
     if (user == null) {
       return done(null, false, { message: 'No user with that email' })
     }
@@ -21,9 +25,13 @@ function initialize(passport, getUserByEmail) {
   passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
   // sepcify what is our user called, by default its username, in this case its email
 
-  passport.serializeUser((user, fone) => {})
-
-  passport.deserializeUser((id, fone) => {})
+  passport.serializeUser((user, done) => {
+    // done(null, user.id)
+  })
+  // null error, save user.id in session, user.id is the serialized version of our user
+  passport.deserializeUser((id, done) => {
+    // return done(null, getUserById(id))
+  })
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -1,18 +1,39 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+  // load all the enviroment variables in development mode
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const users = []
 
 const initializePassport = require('./passport-config')
+
 initializePassport(passport, (email) => {
   return users.find((user) => user.email === email)
 })
 
-const users = []
-
-app.set('view engine', 'ejs')
+app.set('view-engine', 'ejs')
 // use ejs syntax
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    // if notthing is changed, resave no
+    saveUninitialized: false,
+    // save empty value if theres no value
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+// store variables to be persistant across the entire session the user has
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -20,9 +41,22 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.render('login.ejs')
+  console.log(users)
 })
 
-app.post('/login', (req, res) => {})
+app.post(
+  '/login',
+  passport.authenticate(
+    'local',
+    {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true,
+      // flash message, passport-config.js-message
+    }
+    // console.log(users)
+  )
+)
 
 app.get('/register', (req, res) => {
   res.render('register.ejs')
@@ -38,8 +72,9 @@ app.post('/register', async (req, res) => {
       password: hashedPassword,
     })
     res.redirect('/login')
-  } catch {
-    res.redirect('register')
+  } catch (err) {
+    console.log(err)
+    res.redirect('/register')
   }
   console.log(users)
 })
